0c9231e69a15de11345655526bbf16565cfa70e4
Eric Espie <eric.espie@combodo.com>
2022-10-05 14:02:57 +0200

    N°5537 - not able to configure two different mailbox with the same oauth token

--- module.combodo-oauth-email-synchro.php
+++ module.combodo-oauth-email-synchro.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-oauth-email-synchro/1.1.0',
+	'combodo-oauth-email-synchro/1.1.1',
 	array(
 		// Identification
 		//
