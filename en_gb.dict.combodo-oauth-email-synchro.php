8f74164c11f4590af6dcd27c9dd2264fbef648b4
Lars Kaltefleiter <lars.kaltefleiter@itomig.de>
2024-11-07 16:12:12 +0100

    Translation into EN GB

--- en_gb.dict.combodo-oauth-email-synchro.php
+++ en_gb.dict.combodo-oauth-email-synchro.php
@@ -8,7 +8,7 @@
 
 Dict::Add('EN GB', 'British English', 'British English', array(
 	'UI:OAuthEmailSynchro:Wizard:ResultConf:Panel:Title' => 'Create a Mailbox',
-	'UI:OAuthEmailSynchro:Wizard:ResultConf:Panel:Description' => 'Create a new Mailbox to fetch emails from a remote mail provider using this OAuth connection as authentication method',
+	'UI:OAuthEmailSynchro:Wizard:ResultConf:Panel:Description' => 'Create a new Mailbox to fetch emails from a remote mail provider using this OAuth connection as the authentication method.',
 	'UI:OAuthEmailSynchro:Wizard:ResultConf:Panel:CreateNewMailbox' => 'Create a new mailbox',
 
 	'UI:OAuthEmailSynchro:Error:UnknownVendor' => 'OAuth provider %1$s does not exist',
